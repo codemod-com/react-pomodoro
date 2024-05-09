4456e62b6b6ef2dd0e47c479f25969850c810bce
Dmytro Hryshyn <dmytro@codemod.com>
2024-05-09 17:10:52 +0300

    [codemod] react/19/replace-reactdom-render (1 file changed)

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,7 @@
+import { createRoot } from 'react-dom/client';
 import React from 'react';
 import ReactDom from 'react-dom';
 import Pomodoro from './../components/Pomodoro/Pomodoro';
 
-ReactDom.render(<Pomodoro />, document.getElementById('app'));
+const root = createRoot(document.getElementById('app'));
+root.render(<Pomodoro />);
